89646676cfddf0115743504b94801b1741303fc5
Wiktor Kaczor <wiktoraleksanderkaczor@gmail.com>
2025-04-10 06:15:52 +0000

    fix: add JSON Schema refiner for adding required 'additionalProperties' fields

---
+++ RepoTool/Schemas/Refiners/AdditionalPropertiesRefiner.cs
@@ -0,0 +1,24 @@
+using System.Reflection;
+using Json.Schema;
+using Json.Schema.Generation;
+using Json.Schema.Generation.Intents;
+
+public class AdditionalPropertiesRefiner : ISchemaRefiner
+{
+    public bool ShouldRun(SchemaGenerationContextBase context)
+    {
+        bool isObject = context
+            .Intents.OfType<TypeIntent>()
+            .Any(t => t.Type == SchemaValueType.Object);
+        // ⬇️ This is where the magic is. No need to inherit from the base class everywhere.
+        // bool isFromCurrentAssembly = context.Type.Assembly == Assembly.GetExecutingAssembly(); 
+        return isObject; // && isFromCurrentAssembly;
+    }
+
+    public void Run(SchemaGenerationContextBase context) => context.Intents.Add(new AdditionalPropertiesIntent());
+}
+
+public class AdditionalPropertiesIntent() : ISchemaKeywordIntent
+{
+    public void Apply(JsonSchemaBuilder builder) => builder.AdditionalProperties(false);
+}
